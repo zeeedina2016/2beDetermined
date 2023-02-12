26da8be2e9575003dc9cac47a08cf4e8573e9dfc
Elijah Quinones <elijahluisquinones@gmail.com>
2023-02-12 14:18:11 -0500

    made the real Schema for Mongo DB data as well as added the ablity to add data to the data base via a post request with json data to the backend localhost:4000/medrecords

--- api/routes/medrecords.js
+++ api/routes/medrecords.js
@@ -3,12 +3,49 @@ const router = express.Router();
 let med = require('../models/med')
 const medController = require('../controllers/med-controller');
 
-//router.get('/', medController.getMedRecords);
+//get all records
 
 router.route('/').get((req,res) =>{
 med.find({})
   .then(medrecords => res.json(medrecords))
   .catch(err => res.status(400).json('Error: ' + err))
+})
+
+// add a record
+
+router.post('/', async (req,res) => {
+  const{
+    Patient_ID,
+    Age,
+    Sex,
+    Zip,
+    Latest_BMI,
+    Latest_Weight,
+    Png_Filename,
+    Exam_id,
+    ICU_Admit,
+    Num_ICU_admits,
+    Mortality
+
+  } = req.body
+  try{
+    const mrecord = await med.create({
+      Patient_ID,
+      Age,
+      Sex,
+      Zip,
+      Latest_BMI,
+      Latest_Weight,
+      Png_Filename,
+      Exam_id,
+      ICU_Admit,
+      Num_ICU_admits,
+      Mortality
+    })
+    res.status(200).json(mrecord)
+  }catch(error){
+    res.status(400).json({error: error.message})
+  }
 })
 
 module.exports = router;
