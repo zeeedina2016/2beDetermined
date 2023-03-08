abbc2492ef1c7c7ca631ed897a4da2ce3b21a106
zeeedina2016 <zeeedina2016@uri.edu>
2023-03-08 07:21:59 -0500

    took out code for refresh on server.js and index.js file

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+import path from "../../client/public/index.html";
+
+// express app
+const app = express()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('API is working properly!');
 });
+
 
 module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,6 @@ const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/medrecords')
 var indexRouter = require('../Backend/routes/index');
-import path from "../../client/public/index.html";
 
 // express app
 const app = express()
@@ -19,13 +18,8 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/medrecords', workoutRoutes)
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../../client/public/index.html'), function(err) {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
-})
+
+
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
