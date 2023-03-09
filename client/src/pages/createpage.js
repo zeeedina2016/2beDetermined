dfb78ca0ee194473017eb74be63d159a86296814
ElijahQuinones <118852979+ElijahQuinones@users.noreply.github.com>
2023-03-09 12:42:42 -0500

    added Rauls page to main

--- client/src/pages/createpage.js
+++ client/src/pages/createpage.js
@@ -1,78 +1,129 @@
 import '../css/createpage.css';
-import logo from "../medlogo.png";
+import React, { useEffect } from "react"
+import { useParams, useNavigate } from 'react-router-dom'
+import logo from '../medlogo.png'
 
-function Create() {
+export default function Create()
+{
+  const navigate = useNavigate();
+
+  // Sets up initial values. TODO: Use patient id and exam id to fill out info
+  const [values, setValues] = React.useState({
+    Patient_ID: "",
+    Age: "", 
+    Sex: "", 
+    Latest_BMI: "", 
+    Zip: "", 
+    Exam_id: "", 
+    Png_Filename: "",
+    Date: "", 
+    Key_Findings: "",
+    Brixa_Score: ""
+  })
+
+  //handles the changes when user type in input boxs. It changes the value pertaining to the input
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    const temp = {
+      ...values,
+      [name]: value,
+    };
+    setValues(temp);
+  };
+
+  //handles the submit button which is the update button
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const url = "https://atmedrecordsbackend.onrender.com/medrecords/";
+    const response = await fetch(url, {
+      method: 'POST', 
+      body: JSON.stringify(values), 
+      headers: {
+        "Content-Type" : "application/json"
+      }
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+      console.log("Post request successful");
+      console.log(json);
+      navigate("/admin")
+    }
+
+  }
+
+  //handles the cancel button
+  function handleClick(e) {
+    e.preventDefault();
+    console.log("cancel");
+    navigate("/admin")
+  }
+  
   return (
-    <>
-      <div className="create">
+    <div className="create">
+        <h2>CREATE PAGE</h2>
         <div className="wrapper"> 
         <img src={logo} alt="medlogo" />
-        <h2>CREATE PAGE</h2>
         </div>
-        
+
+        <form onSubmit={handleSubmit}>
         <div className='patientInfoDiv'>
           <div>
             <h2>Patient Info</h2>
-        
+
             <label>Patient ID:</label>
             <div>
-              <input className="textFields" type="text"></input>
+              <input name='Patient_ID' className="textFields" value={values.Patient_ID} onChange={handleInputChange}></input>
             </div>
-    
-            
             
             <label>Age:</label>
             <div>
-              <input className="textFields" type="text"></input>
+              <input name='Age' className="textFields" value={values.Age} onChange={handleInputChange}></input>
             </div>
             
             <label>Sex:</label>
             <div>
-              <input className="textFields" type="text"></input>
+              <input name='Sex' className="textFields" value={values.Sex} onChange={handleInputChange}></input>
             </div>
             
             <label>BMI:</label>
             <div>
-              <input className="textFields" type="text"></input>
+              <input name='Latest_BMI' className="textFields" value={values.Latest_BMI} onChange={handleInputChange}></input>
             </div>
             
             <label>Zip Code:</label>
             <div>
-              <input className="textFields" type="text"></input>
+              <input name='Zip' className="textFields" value={values.Zip} onChange={handleInputChange}></input>
             </div>
           </div>
           
-          
           <div>
-          <h2>Exam Info</h2>
-          <label>Exam ID:</label>
+            <h2>Exam Info</h2>
+            <label>Exam ID:</label>
             <div>
-              <input className="textFields" type="text"></input>
+              <input name='Exam_id' className="textFields" value={values.Exam_id} onChange={handleInputChange}></input>
             </div>
             
             <label>Image URL:</label>
             <div>
-              <input className="textFields" type="text"></input>
+              <input name='Png_Filename' className="textFields" value={values.Png_Filename} onChange={handleInputChange}></input>
             </div>
             
             <label>Brixia Score:</label>
             <div>
-              <input className="textFields" type="text"></input>
+              <input name='Brixa_Score' className="textFields" value={values.Brixa_Score} onChange={handleInputChange}></input>
             </div>
             <label>Key Findings:</label>
             <div >
-              <input className="textFields" type="text"></input>
+              <input name='Key_Findings' className="textFields" value={values.Key_Findings} onChange={handleInputChange}></input>
             </div>
-        </div>
+          </div>
         </div>
         <div className='buttonsContainer'>
-          <button className="blueButton">Add Exam</button>
+          <button className="blueButton" type='submit'>Add Exam</button>
           <button className="redButton">Cancel</button>
         </div>
-
-      </div>
-     
-    </>
+      </form>
+    </div>
   );
 }
-export default Create;
