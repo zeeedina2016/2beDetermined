a8825a3005bc933557d1a573b7d0dacca5fe2965
ElijahQuinones <118852979+ElijahQuinones@users.noreply.github.com>
2023-03-09 22:12:57 -0500

    Update exampage.js

--- client/src/pages/exampage.js
+++ client/src/pages/exampage.js
@@ -1,143 +1,100 @@
-import '../css/exampage.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App.css';
-// import data from '../fakeData.json';
-// import Container from 'react-bootstrap/Container';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-boostrap/Col';
+import '../css/exampage.css';
+import { useParams } from 'react-router-dom';
 
-function Exam() {
-    // const [patient, setPatient] = useState({
-    //     id: "",
-    //     age: "",
-    //     sex: "",
-    //     bmi: "",
-    //     zipCode: "",
-    //     examID: "",
-    //     imageURL: "https://ohif-hack-diversity-covid.s3.amazonaws.com/covid-png/COVID-19-AR-16434381_XR_CHEST_AP_ONLY_1.png",
-    //     date: "",
-    //     keyFindings: "",
-    // });
-
-    const [updatePatient, setUpdatePatient] = useState(false);
-    const [updateExam, setUpdateExam] = useState(false);
-
-    /* I tried to fetch data from a fake json file, but doesn't seem to work :( */
-    // useEffect(() => {
-    //     setPatient(data)
-    //     console.log(data)
-    // }, []);
-
-    // if (!patient) {
-    //     return <h1>Loading...</h1>
-    // }
-
+function Exam(props) {
     const [patient, setPatient] = useState({
-        id: "COVID-19-AR-16406513",
-        age: 44,
-        sex: "M",
-        bmi: 32.3,
-        zipCode: 722,
-        examID: "Exam-3",
-        imageURL: "https://ohif-hack-diversity-covid.s3.amazonaws.com/covid-png/COVID-19-AR-16434381_XR_CHEST_AP_ONLY_1.png",
-        date: "2023-01-01",
-        keyFindings: "",
+        Patient_ID: "",
+        Age: "",
+        Sex: "",
+        Latest_BMI: "",
+        Zipcode: "",
+        Exam_id: "",
+        Png_Filename: "",
+        Date: "",
+        Key_Findings: "",
+        Brixa_Score: "",
     });
 
-    // const [getPatient, setGetPatient] = useState({
-    //     id = {data.patientid},
-    //     age = {data.age},
-    //     sex = {data.sex},
-    //     bmi = {data.bmi},
-    //     zipCode = {data.zipcode},
-    //     examID = {data.examid},
-    //     imageURL = "https://ohif-hack-diversity-covid.s3.amazonaws.com/covid-png/COVID-19-AR-16434381_XR_CHEST_AP_ONLY_1.png",
-    //     date = "2023-01-01",
-    //     keyFindings = "",
-    // });
+    const {id} = useParams()
 
-
-
-    // const [patient, setPatient] = useState({
-    //         id: {data.patientid},
-    //         age: {data.age},
-    //         sex: {data.sex},
-    //         bmi: {data.bmi},
-    //         zipCode: {data.zipcode},
-    //         examID: {data.examid},
-    //         imageURL: "https://ohif-hack-diversity-covid.s3.amazonaws.com/covid-png/COVID-19-AR-16434381_XR_CHEST_AP_ONLY_1.png",
-    //         date: "2023-01-01",
-    //         keyFindings: "",
-    // });
-
-    const updatePatientInfo = () => {
-        setUpdatePatient(!updatePatient)
-    };
-
-    const updateExamInfo = () => {
-        setUpdateExam(!updateExam)
-    };
+    useEffect(() => {
+        const fetchMedRecords = async () => {
+          const url = 'https://atmedrecordsbackend.onrender.com/medrecords/' + id
+          const response = await fetch(url)
+          const json = await response.json()
+    
+          console.log(json)
+    
+          if (response.ok) {
+            setPatient({
+              Patient_ID: (json.Patient_ID == undefined) ? "" : json.Patient_ID,
+              Age: (json.Age == undefined) ? "" : json.Age, 
+              Sex: (json.Sex == undefined) ? "" : json.Sex,
+              Latest_BMI: (json.Latest_BMI == undefined) ? "" : json.Latest_BMI, 
+              Zip: (json.Zip == undefined) ? "" : json.Zip, 
+              Exam_id: (json.Exam_id == undefined) ? "" : json.Exam_id, 
+              Png_Filename: (json.Png_Filename == undefined) ? "" : json.Png_Filename, 
+              Date: (json.Date == undefined) ? "" : json.Date, 
+              Key_Findings: (json.Key_Findings == undefined) ? "" : json.Key_Findings, 
+              Brixa_Score: (json.Brixa_Score == undefined) ? "" : json.Brixa_Score, 
+            })
+          }
+        }
+    
+        fetchMedRecords(id)
+      }, [id])
 
     return (
-        <div className="page">
-            <div className="col1">
+
+        <div>
+            <div>
                 <h1 className="col1_H">Patient Information</h1>
-                <>
+                <div>
                     <h4 className="patientID_H">Patient ID</h4>
-                    <div className="patientID">{patient.id}</div>
+                    <div className="patientID">{patient.Patient_ID}</div>
 
                     <h4 className="age_H">Age</h4>
-                    <div className="age">{patient.age} </div>
+                    <div className="age">{patient.Age} </div>
 
                     <h4 className="sex_H">Sex</h4>
-                    <div className="sex">{patient.sex}</div>
+                    <div className="sex">{patient.Sex}</div>
 
                     <h4 className="bmi_H">BMI</h4>
-                    <div className="bmi">{patient.bmi}</div>
+                    <div className="bmi">{patient.Latest_BMI}</div>
 
                     <h4 className="zipCode_H">ZipCode</h4>
-                    <div className="zipCode">{patient.zipCode}</div>
-                </>
-
-                {/* If pressed, go to Edit page ? */}
-                <button className="buttonPatient" onClick={updatePatientInfo}>UPDATE</button>
+                    <div className="zipCode">{patient.Zip}</div>
+                </div>
             </div>
 
-            <div className="col2">
+            <div>
                 <h1 className="col2_H">Exam Information</h1>
-                <>
+                <div>
                     <img 
                         className="img"
-                        src={patient.imageURL}
-                        alt={patient.imageURL}
+                        src={"https://ohif-hack-diversity-covid.s3.amazonaws.com/covid-png/" + patient.Png_Filename}
+                        alt={""}
                     />
 
                     <h4 className="examID_H">Exam ID</h4>
-                    <div className="examID">{patient.examID}</div>
+                    <div className="examID">{patient.Exam_id}</div>
 
-                    {/* Will likely useState here once styling */}
-                    <h4 className="subObjectHeadings">Date</h4>
+                    <h4 className="date_H">Date</h4>
+                    <div className="date">{patient.Date}</div>
+
+                    <h4 className="url_H">Image URL</h4>
+                    <div className="url">{patient.Png_Filename}</div>
+
+                    <h4 className="keyFindings_H">Exam Details</h4>
                     <input 
-                        className="subObjectData"
-                        type="date"
-                        value={patient.date}
-                        onChange={e => setPatient({...patient, date: e.target.value})}
-                    />
-
-                    <h4 className="subObjectHeadings">Image URL</h4>
-                    <div className="subObjectData">{patient.imageURL}</div>
-
-                    <h4 className="subObjectHeadings">Exam Details</h4>
-                    <input 
-                        className="subObjectData"
+                        className="keyFindings"
                         type="text"
-                        value={patient.keyFindings}
-                        onChange={e => setPatient({...patient, keyFindings: e.target.value})}
+                        value={patient.Key_Findings}
+                        onChange={e => setPatient({...patient, Key_Findings: e.target.value})}
                     />
-                </>
-
-                {/* If pressed, go to Edit page ? */}
-                <button className="buttonExam" onClick={updateExamInfo}>UPDATE</button>
+                </div>
             </div>
         </div>
     );
