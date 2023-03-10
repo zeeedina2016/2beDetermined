3cf762b9d6e9c23c0ec2f4ac6bf4b0cb44d451f7
karell19 <113552974+karell19@users.noreply.github.com>
2023-03-09 19:00:00 -0500

    Cleaning up

--- client/src/examPage/examPage.js
+++ client/src/examPage/examPage.js
@@ -20,75 +20,6 @@ function Exam(props) {
         Brixa_Score: "",
     });
 
-    // const [updatePatient, setUpdatePatient] = useState(false);
-    // const [updateExam, setUpdateExam] = useState(false);
-
-    /* I tried to fetch data from a fake json file, but doesn't seem to work :( */
-    // useEffect(() => {
-    //     setPatient(data)
-    //     console.log(data)
-    // }, []);
-
-
-
-    // const [patient, setPatient] = useState({
-    //     id: "COVID-19-AR-16406513",
-    //     age: 44,
-    //     sex: "M",
-    //     bmi: 32.3,
-    //     zipCode: 722,
-    //     examID: "Exam-3",
-    //     imageURL: "https://ohif-hack-diversity-covid.s3.amazonaws.com/covid-png/COVID-19-AR-16434381_XR_CHEST_AP_ONLY_1.png",
-    //     date: "2023-01-01",
-    //     keyFindings: "",
-    // });
-
-    // if (!patient) {
-    //     return <h1>Loading...</h1>
-    // }
-
-    // const [getPatient, setGetPatient] = useState({
-    //     id = {data.patientid},
-    //     age = {data.age},
-    //     sex = {data.sex},
-    //     bmi = {data.bmi},
-    //     zipCode = {data.zipcode},
-    //     examID = {data.examid},
-    //     imageURL = "https://ohif-hack-diversity-covid.s3.amazonaws.com/covid-png/COVID-19-AR-16434381_XR_CHEST_AP_ONLY_1.png",
-    //     date = "2023-01-01",
-    //     keyFindings = "",
-    // });
-
-
-
-    // const [patient, setPatient] = useState({
-    //         id: {data.patientid},
-    //         age: {data.age},
-    //         sex: {data.sex},
-    //         bmi: {data.bmi},
-    //         zipCode: {data.zipcode},
-    //         examID: {data.examid},
-    //         imageURL: "https://ohif-hack-diversity-covid.s3.amazonaws.com/covid-png/COVID-19-AR-16434381_XR_CHEST_AP_ONLY_1.png",
-    //         date: "2023-01-01",
-    //         keyFindings: "",
-    // });
-
-    // const updatePatientInfo = () => {
-    //     setUpdatePatient(!updatePatient)
-    // };
-
-    // const updateExamInfo = () => {
-    //     setUpdateExam(!updateExam)
-    // };
-
-    // const getData = () => {
-    //     fetch("http://localhost:4000/medrecords/")
-    //         .then((res) => res.json())
-    //         .then((data) => {
-    //             console.log(data);
-    //     });
-    // };
-
     const id = props.id
 
     useEffect(() => {
@@ -118,12 +49,6 @@ function Exam(props) {
         fetchMedRecords(id)
       }, [id])
 
-    // const getData = () => {
-    //     Axios.get("http://localhost:4000/medrecords/").then((res) => {
-    //         console.log(data);
-    //     })
-    // }
-
     return (
 
         <div>
@@ -145,9 +70,6 @@ function Exam(props) {
                     <h4 className="zipCode_H">ZipCode</h4>
                     <div className="zipCode">{patient.Zipcode}</div>
                 </div>
-
-                {/* If pressed, go to Edit page ? */}
-                {/* <button className="buttonPatient" onClick={updatePatientInfo}>UPDATE</button> */}
             </div>
 
             <div>
@@ -162,14 +84,8 @@ function Exam(props) {
                     <h4 className="examID_H">Exam ID</h4>
                     <div className="examID">{patient.Exam_id}</div>
 
-                    {/* Will likely useState here once styling */}
                     <h4 className="date_H">Date</h4>
-                    {/* <input 
-                        className="date"
-                        type="date"
-                        value={patient.date}
-                        onChange={e => setPatient({...patient, Date: e.target.value})}
-                    /> */}
+                    <div className="date">{patient.Date}</div>
 
                     <h4 className="url_H">Image URL</h4>
                     <div className="url">{patient.Png_Filename}</div>
@@ -182,9 +98,6 @@ function Exam(props) {
                         onChange={e => setPatient({...patient, Key_Findings: e.target.value})}
                     />
                 </div>
-
-                {/* If pressed, go to Edit page ? */}
-                {/* <button className="buttonExam" onClick={updateExamInfo}>UPDATE</button> */}
             </div>
         </div>
     );
